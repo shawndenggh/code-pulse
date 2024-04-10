48abdbba76640ab2872b6bd880e68df2da6a9e7f
Shawn Deng <dengguiheng@apitable.com>
2024-04-10 23:00:18 +0800

    add eslint

--- src/cancan/index.ts
+++ src/cancan/index.ts
@@ -3,13 +3,34 @@ import CanCan from 'cancan';
 const cancan = new CanCan();
 const {allow, can, cannot, authorize} = cancan;
 
-class User{}
-class Article{}
+class User{
+    private readonly _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+class Article{
+
+    private readonly _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
 
 allow(User, 'read', Article);
 
-const user = new User();
-const article = new Article();
+const user = new User('shawn');
+const article = new Article('Animal');
 
 const canRead = can(user, 'read', article);
 console.log(`canRead: ${canRead}`);
