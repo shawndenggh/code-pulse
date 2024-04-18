8e4ee1268f894f0628b8e86eb03e2279a27c1116
Shawn Deng <dengguiheng@apitable.com>
2024-04-18 15:41:28 +0800

    test

--- src/casl/abilities.ts
+++ src/casl/abilities.ts
@@ -23,26 +23,59 @@ class Role {
 class User {
   id: number;
   isAdmin: boolean;
-  role: Roles;
-  teams: Team[];
-  roles: Role[];
 
-  constructor(id: number, isAdmin: boolean, role: Roles, teams: Team[], roles: Role[]) {
+  constructor(id: number, isAdmin: boolean) {
     this.id = id;
     this.isAdmin = isAdmin;
-    this.role = role;
-    this.teams = teams;
-    this.roles = roles;
+  }
+
+  async getTeams(): Promise<Team[]> {
+    return Promise.resolve([new Team(1, 'HR')]);
+  }
+
+  async getRoles(): Promise<Role[]> {
+    return Promise.resolve([new Role(1, 'Sales')]);
+  }
+
+  async getPrivilege(nodeId: string): Promise<Privilege> {
+    const privileges = permissions.filter((p) => p.userId === this.id && p.nodeId === nodeId);
+    return Promise.resolve(privileges[0].privilege);
   }
 }
 
 class Node {
+  id: string;
   sharing: boolean;
-  memberships: Record<number, Roles>;
 
-  constructor(sharing: boolean, memberships: Record<string, Roles>) {
+  constructor(id: string, sharing: boolean) {
+    this.id = id;
     this.sharing = sharing;
-    this.memberships = memberships;
+  }
+
+  async getPrivileges(userId: number): Promise<Privilege> {
+    const privileges = permissions.filter((p) => p.userId === userId);
+    return Promise.resolve(privileges[0].privilege);
+  }
+}
+
+class Permission {
+  userId: number;
+  nodeId: string;
+  privilege: Privilege;
+
+  constructor(userId: number, nodeId: string, privilege: Privilege) {
+    this.userId = userId;
+    this.nodeId = nodeId;
+    this.privilege = privilege;
+  }
+}
+
+class Database {
+  // user_id => privilege
+  privileges: Record<number, Privilege>;
+
+  constructor(privileges: Record<number, Privilege>) {
+    this.privileges = privileges;
   }
 }
 
@@ -54,8 +87,13 @@ function or(...args: boolean[]) {
   return args.some(Boolean);
 }
 
-type Actions = 'all' | 'read' | 'create' | 'edit' | 'update' | 'delete' | 'move' | 'export';
-type Subjects = typeof Node | Node;
+// const nodeActions = ['all', 'read', 'create', 'edit', 'update', 'delete', 'move', 'export'];
+
+type NodeActions = 'all' | 'read' | 'create' | 'edit' | 'update' | 'delete' | 'move' | 'export';
+type DatabaseActions = 'read' | 'update' | 'delete';
+
+type Actions = NodeActions | DatabaseActions;
+type Subjects = typeof Node | Node | typeof Database | Database;
 
 // const actions = ['update', 'manage', 'invite'] as const;
 // const subjects = ['User', 'all'] as const;
@@ -65,14 +103,18 @@ type AppAbility = MongoAbility<[Actions, Subjects]>;
 const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
 
 type DefinePermissions = (user: User, builder: AbilityBuilder<AppAbility>) => void;
-type Roles = 'FULL_ACCESS' | 'CAN_EDIT';
+type Privilege = 'FULL_ACCESS' | 'CAN_EDIT' | 'CAN_VIEW';
 
-const rolePermissions: Record<Roles, DefinePermissions> = {
+const rolePermissions: Record<Privilege, DefinePermissions> = {
+  CAN_VIEW(user, { can }) {
+    can('read', Node);
+  },
   CAN_EDIT(user, { can }) {
-    can('edit', Node, { memberships: { [user.id]: 'CAN_EDIT' } });
+    can('edit', Node);
   },
   FULL_ACCESS(user, { can }) {
-    can('all', Node, { memberships: { [user.id]: 'FULL_ACCESS' } });
+    // 判断privileges中是否有user.id,并且权限为FULL_ACCESS
+    can('all', Node);
   },
 };
 
@@ -81,13 +123,26 @@ const resolveAction = createAliasResolver({
   all: ['create', 'edit', 'delete', 'move', 'export'],
 });
 
-function defineAbilityFor(user: User): AppAbility {
+async function defineAbilityFor(user: User, nodeId: string): Promise<AppAbility> {
   const builder = new AbilityBuilder(createAppAbility);
 
-  if (typeof rolePermissions[user.role] === 'function') {
-    rolePermissions[user.role](user, builder);
+  // if user is admin, give full access
+  if (user.isAdmin) {
+    builder.can('all', Node);
+    return builder.build({ resolveAction });
+  }
+
+  // fetch this user's team and role
+  const teams = await user.getTeams();
+  const roles = await user.getRoles();
+
+  // fetch node's privilege for this user
+  const privilege = await user.getPrivilege(nodeId);
+
+  if (typeof rolePermissions[privilege] === 'function') {
+    rolePermissions[privilege](user, builder);
   } else {
-    throw new Error(`Trying to use unknown role "${user.role}"`);
+    throw new Error(`Trying to use unknown role "${privilege}"`);
   }
 
   return builder.build({ resolveAction });
@@ -95,37 +150,69 @@ function defineAbilityFor(user: User): AppAbility {
 
 // test
 
-const hr = new Team(1, 'HR');
-const dev = new Team(2, 'Dev');
+const teams: Record<string, Team> = {
+  hr: new Team(1, 'HR'),
+  dev: new Team(2, 'Dev'),
+  product: new Team(3, 'Product'),
+};
 
-const sales = new Role(1, 'Sales');
+const roles: Record<string, Role> = {
+  sales: new Role(1, 'Sales'),
+  market: new Role(2, 'Market'),
+};
 
-const shawn = new User(1, true, 'FULL_ACCESS', [hr], [sales]);
-const kelvin = new User(2, true, 'FULL_ACCESS', [dev], []);
+const users: Record<string, User> = {
+  shawn: new User(1, true),
+  kelvin: new User(2, false),
+  benson: new User(3, false),
+};
 
-const target = new Node(false, {
-  '1': 'CAN_EDIT',
-  '2': 'FULL_ACCESS',
-});
+// equal same as in permission table in database
+const permissions: Permission[] = [
+  new Permission(1, 'table-1', 'FULL_ACCESS'),
+  new Permission(2, 'table-1', 'CAN_EDIT'),
+  new Permission(3, 'table-1', 'CAN_VIEW'),
+];
 
-const ability = defineAbilityFor(shawn);
+const shawn = users.shawn;
+const kelvin = users.kelvin;
+const benson = users.benson;
 
-const canCreate = ability.can('create', target);
+const target = new Node('table-1', false);
 
-console.log(`canCreate: ${canCreate}`);
-
-const canUpdate = ability.can('update', target);
-
-console.log(`canUpdate: ${canUpdate}`);
-
-ability.rules.forEach((rule) => {
-  console.log(rule.action);
-  console.log(typeof rule.action);
-  if (typeof rule.action !== 'string') {
-    rule.action.forEach((action) => {
-      console.log(`action: ${action} - ${ability.can(action, target)}`);
+function serialize(ability: AppAbility, target: Node | Database): Record<string, boolean> {
+  const output: Record<string, boolean> = {};
+  ability.rules
+    .filter((rule) => rule.subject === target.constructor)
+    .forEach((rule) => {
+      // console.log(rule.action);
+      if (typeof rule.action !== 'string') {
+        rule.action.forEach((action) => {
+          // console.log(`action: ${action} - ${ability.can(action, target)}`);
+          output[action] = ability.can(action, target);
+        });
+      } else {
+        // console.log(`action: ${rule.action} - ${ability.can(rule.action, target)}`);
+        const canDo = ability.can(rule.action, target);
+        output[rule.action] = canDo;
+        resolveAction(rule.action).forEach((subAction) => {
+          // console.log(`subAction: ${subAction}`);
+          output[subAction] = canDo;
+        });
+      }
     });
-  } else {
-    console.log(`action: ${rule.action} - ${ability.can(rule.action, target)}`);
-  }
-});
+  // iterate Actions
+  return output;
+}
+
+console.log(`Shawn`);
+const abilityForShawn = await defineAbilityFor(shawn, target.id);
+console.log(serialize(abilityForShawn, target));
+
+console.log(`Kelvin`);
+const abilityForKelvin = await defineAbilityFor(kelvin, target.id);
+console.log(serialize(abilityForKelvin, target));
+
+console.log(`Benson`);
+const abilityForBenson = await defineAbilityFor(benson, target.id);
+console.log(serialize(abilityForBenson, target));
